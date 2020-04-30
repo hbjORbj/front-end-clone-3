4f1cdd5f3a2245f061c2df3240ecb137f7eea5c0
hbjORbj <sldisek783@gmail.com>
2020-04-30 02:07:43 +0100

    Update 2020-04-30T01:07:30.139Z

--- js/index.js
+++ js/index.js
@@ -1,133 +1,142 @@
 var time = 500,
-    images = [];
-var i = 0,
-    imageNumber = 1;
+  images = [];
+var i1 = 0,
+  i2 = 0,
+  i3 = 0,
+  i4 = 0,
+  i5 = 0,
+  i6 = 0,
+  i7 = 0,
+  i8 = 0,
+  i9 = 0,
+  i10 = 0;
+imageNumber = 1;
 var tOut = null;
 
 for (k = 1; k <= 10; k++) {
   images[k] = [];
 
   for (j = 0; j < 5; j++) {
-    images[k][j] = "images/"+imageNumber+".jpg";
+    images[k][j] = "images/".concat(imageNumber, ".jpg");
     imageNumber++;
   }
 }
 
 var changeImg1 = function changeImg1() {
-  document.slide.src = images[1][i];
+  document.slide.src = images[1][i1];
 
-  if (i < images[1].length - 1) {
-    i++;
+  if (i1 < images[1].length - 1) {
+    i1++;
   } else {
-    i = 0;
+    i1 = 0;
   }
 
   tOut = setTimeout("changeImg1()", time);
 };
 
 var changeImg2 = function changeImg2() {
-  document.slide.src = images[2][i];
+  document.slide.src = images[2][i2];
 
-  if (i < images[2].length - 1) {
-    i++;
+  if (i2 < images[2].length - 1) {
+    i2++;
   } else {
-    i = 0;
+    i2 = 0;
   }
 
   tOut = setTimeout("changeImg2()", time);
 };
 
 var changeImg3 = function changeImg3() {
-  document.slide.src = images[3][i];
+  document.slide.src = images[3][i3];
 
-  if (i < images[3].length - 1) {
-    i++;
+  if (i3 < images[3].length - 1) {
+    i3++;
   } else {
-    i = 0;
+    i3 = 0;
   }
 
   tOut = setTimeout("changeImg3()", time);
 };
 
 var changeImg4 = function changeImg4() {
-  document.slide.src = images[4][i];
+  document.slide.src = images[4][i4];
 
-  if (i < images[4].length - 1) {
-    i++;
+  if (i4 < images[4].length - 1) {
+    i4++;
   } else {
-    i = 0;
+    i4 = 0;
   }
 
   tOut = setTimeout("changeImg4()", time);
 };
 
 var changeImg5 = function changeImg5() {
-  document.slide.src = images[5][i];
+  document.slide.src = images[5][i5];
 
-  if (i < images[5].length - 1) {
-    i++;
+  if (i5 < images[5].length - 1) {
+    i5++;
   } else {
-    i = 0;
+    i5 = 0;
   }
 
   tOut = setTimeout("changeImg5()", time);
 };
 
 var changeImg6 = function changeImg6() {
-  document.slide.src = images[6][i];
+  document.slide.src = images[6][i6];
 
-  if (i < images[6].length - 1) {
-    i++;
+  if (i6 < images[6].length - 1) {
+    i6++;
   } else {
-    i = 0;
+    i6 = 0;
   }
 
   tOut = setTimeout("changeImg6()", time);
 };
 
 var changeImg7 = function changeImg7() {
-  document.slide.src = images[7][i];
+  document.slide.src = images[7][i7];
 
-  if (i < images[7].length - 1) {
-    i++;
+  if (i7 < images[7].length - 1) {
+    i7++;
   } else {
-    i = 0;
+    i7 = 0;
   }
 
   tOut = setTimeout("changeImg7()", time);
 };
 
 var changeImg8 = function changeImg8() {
-  document.slide.src = images[8][i];
+  document.slide.src = images[8][i8];
 
-  if (i < images[8].length - 1) {
-    i++;
+  if (i8 < images[8].length - 1) {
+    i8++;
   } else {
-    i = 0;
+    i8 = 0;
   }
 
   tOut = setTimeout("changeImg8()", time);
 };
 
 var changeImg9 = function changeImg9() {
-  document.slide.src = images[9][i];
+  document.slide.src = images[9][i9];
 
-  if (i < images[9].length - 1) {
-    i++;
+  if (i9 < images[9].length - 1) {
+    i9++;
   } else {
-    i = 0;
+    i9 = 0;
   }
 
   tOut = setTimeout("changeImg9()", time);
 };
 
 var changeImg10 = function changeImg10() {
-  document.slide.src = images[10][i];
+  document.slide.src = images[10][i10];
 
-  if (i < images[10].length - 1) {
-    i++;
+  if (i10 < images[10].length - 1) {
+    i10++;
   } else {
-    i = 0;
+    i10 = 0;
   }
 
   tOut = setTimeout("changeImg10()", time);
@@ -136,6 +145,4 @@ var changeImg10 = function changeImg10() {
 var changeStop = function changeStop() {
   clearTimeout(tOut);
   tOut = null;
-  i = 0;
 };
-
